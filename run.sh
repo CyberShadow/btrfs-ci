91cd3fb74df836694f344e827fb2a09d0a532121
Vladimir Panteleev <git@thecybershadow.net>
2019-08-19 20:21:23 +0000

    Build rootfs image

--- run.sh
+++ run.sh
@@ -53,6 +53,38 @@ function build_kernel() {
 	fi
 }
 
+function build_image() {
+	rm -rf "$work_dir"/root
+	mkdir -p "$work_dir"/root
+
+	local image_build_id="$image_arch_date"-"$self_sha1"
+	image="$cache_dir"/image-"$image_build_id"
+	if [[ ! -f "$image" ]]
+	then
+		# debootstrap has a hard requirement on root, so we can't use Debian.
+		# Instead use Arch Linux, for which fakeroot and fakechroot suffice.
+		WORK_DIR="$work_dir" ARCH_DATE="$image_arch_date" IMAGE="$image" FAKECHROOT_EXCLUDE_PATH=/dev:/proc:/sys fakeroot fakechroot bash -s <<-'IMAGEEOF'
+			set -xeEuo pipefail
+			curl "https://archive.archlinux.org/iso/$ARCH_DATE/archlinux-bootstrap-$ARCH_DATE-x86_64.tar.gz" |
+				tar zxv -C "$WORK_DIR"/root
+
+			chroot "$WORK_DIR"/root/root.x86_64 bash -s <<-'ARCHEOF'
+				set -xeEuo pipefail
+				pacman-key --init
+				pacman-key --populate archlinux
+				echo "Server=https://archive.archlinux.org/repos/${ARCH_DATE//.//}/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+				pacman -Sy
+				pacman -S --noconfirm btrfs-progs
+			ARCHEOF
+
+			dd if=/dev/zero of="$IMAGE".tmp bs=1G count=0 seek=1
+			mkfs.ext4 "$IMAGE".tmp -d "$WORK_DIR"/root/root.x86_64
+			rm -rf "$WORK_DIR"/root
+			mv "$IMAGE".tmp "$IMAGE"
+		IMAGEEOF
+	fi
+}
+
 ### Configuration
 
 top=$PWD
@@ -72,3 +104,4 @@ mkdir -p "$cache_dir"
 ### Main
 
 build_kernel
+build_image
